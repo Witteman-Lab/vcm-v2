e45dbbb8a38e6e37a0b5bc6c74985efcc830b0b9
Aminata48 <aminata.camara.2@ulaval.ca>
2022-08-04 15:06:56 -0400

    affichage des deux langues

---
+++ src/components/Navigation.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="labels">
+      <button class="languageBtn"
+             @click="$emit('langswitch')"
+      >{{ labels.currentLangue }}</button>
+    </div>
+  </div>
+</template>
+<script>
+import config from "../assets/json/config.json"
+
+export default {
+  name: 'Navigation',
+  components: {
+  },
+
+  data() {
+    return {
+      config
+    }
+  },
+  props: ['labels'],
+
+}
+</script>
+<style lang="scss">
+
+.labels {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+
+}
+.languageBtn {
+  border: solid;
+  /* border-radius: 35px 35px 35px 35px; */
+  transition: 0.3s;
+  height: 30px;
+  width: 150px;
+}
+</style>
